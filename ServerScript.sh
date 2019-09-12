c34336428fab81d0b42856008028190d4c46b831
Yogendrasingh-Rathore <53259324+Yogendrasingh-Rathore@users.noreply.github.com>
2019-09-12 23:20:44 +0530

    Update ServerScript.sh

--- ServerScript.sh
+++ ServerScript.sh
@@ -1,6 +1,6 @@
 cd ./Server
-#docker volume create servervol
-#docker network create -d bridge MyNet --subnet="10.11.0.0/12"
+docker volume create servervol
+docker network create -d bridge MyNet --subnet="10.11.0.0/12"
 docker build . -t serverscript
 docker run --name ServerCont -v servervol:/serverdata --net MyNet --ip=10.11.0.2 -it serverscript 
 
